9a8187e51c280415654f338cf6fafcb78d0d1220
LathaThavamai <latha.thavamani@gmail.com>
2023-04-07 14:22:30 +0200

    Dashboard-Implemented with Header,content & Footer

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Home from '../components/Home'
-//import Dashboad from './../features/Dashboard/Dashboard';
+import Dashboad from './../features/Dashboard/Dashboard';
 import Signin from './../features/Signin/Signin'
 import Signup from './../features/Signup/Signup'
 
@@ -11,7 +11,7 @@ const AppRoutes = () => {
             <Route path='/' element={<Home />} ></Route>
             <Route path='/signin' element={<Signin />}></Route>
             <Route path='/signup' element={<Signup />}></Route>
-            {/* <Route path='/dashboard' element={<Dashboad />}></Route> */}
+            <Route path='/dashboard' element={<Dashboad />}></Route>
         </Routes>
     )
 }
---
+++ src/features/Dashboard/Dashboard.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from 'react';
+import '../../styles/Dashboard.css';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import { Layout } from './components/Layout';
+
+//import { getSearchSuccess } from '../../Redux/Search/action'
+//import { useDispatch, useSelector } from 'react-redux'
+//import MovieRow from './components/MovieRow';
+//import { makeGetMoviesRequest } from '../../Redux/Movies/action.js';
+//import { makeGetSeriesRequest } from '../../Redux/TvShows/action';
+//import { Search } from '../Search/Search';
+//import { getProfiles, setCurrentProfile } from '../../Redux/Profile/actions/profileActions';
+//import Loader from '../../Components/Loader/Loader';
+
+
+
+
+function Dashboad(props) {
+    const [blackHeader, setBlackHeader] = useState(false);
+    const [mute, setMute] = useState(true);
+    const [play, setPlay] = useState(true);
+    // const dispatch = useDispatch();
+    // const { movies, isLoading } = useSelector(state => state.movies)
+    // const { series } = useSelector(state => state.series)
+    // const { searchResults: searchList } = useSelector((state) => state.search)
+    // const { currentProfile } = useSelector(state => state.profiles)
+
+
+    // useEffect(() => {
+    //     dispatch(makeGetMoviesRequest())
+    //     dispatch(makeGetSeriesRequest())
+    //     dispatch(getSearchSuccess([]))
+    //     if (!currentProfile) {
+    //         let token = localStorage.getItem("token")
+    //         dispatch(getProfiles(token))
+    //         dispatch(setCurrentProfile(JSON.parse(localStorage.getItem("currentProfile"))))
+    //     }
+
+    // }, []);
+
+    // useEffect(() => {
+    //     const scrollListener = () => {
+    //         if (window.scrollY > 10) {
+    //             setBlackHeader(true);
+    //         } else {
+    //             setBlackHeader(false);
+    //         }
+    //     }
+
+
+    //     window.addEventListener('scroll', scrollListener);
+    //     return () => {
+    //         window.removeEventListener('scroll', scrollListener);
+    //     }
+    // }, []);
+
+
+
+    return (
+        <Layout>
+
+            {
+                //isLoading ? <Loader /> :
+                //searchList.length === 0 ?
+                <div className="page">
+
+                    {/* <Header black={blackHeader} search={search} setSearch={setSearch} /> */}
+                    <div className="root">
+
+                        <div className="player">
+                            <video aloop="1" autoPlay={play} muted={mute} width="100%"
+                                poster="https://i.ytimg.com/vi/htvVnHnroQ4/maxresdefault.jpg"
+                                src="https://mphomeservices.it/videos/Ad%20Astra%20_%20Official%20Trailer%20%5BHD%5D%20_%2020th%20Century%20FOX.mp4"
+                            >
+                            </video>
+                            <div className="video-info">
+                                <div className="synopsis">
+                                    <p>
+                                        A large and lovable rabbit deals with three tiny bullies, led by a flying squirrel, who are determined to squelch his happiness.
+                                    </p>
+                                    <button className="play_btn" >
+                                        <i className="Icon fa fa-play play" />
+                                        Play
+                                    </button>
+                                    <button >
+                                        <i className="Icon fa fa-info-circle info-circle" />
+                                        More Info
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="video-info-right">
+                                <div
+                                    onClick={() => setMute(prev => !prev)}
+                                >
+                                    {!mute ? <VolumeUpIcon style={{
+                                        color: '#fff',
+                                        cursor: 'pointer'
+                                    }} />
+                                        :
+                                        <VolumeOffIcon style={{
+                                            color: '#fff',
+                                            cursor: 'pointer'
+                                        }} />}
+                                </div>
+                            </div>
+                            <div
+                                className="video-bottom"
+                            >
+
+                            </div>
+                        </div>
+                    </div>
+                    {/* {
+                        movies.length ? <section className="lists">
+                            {movies.map((item, key) => (
+                                <MovieRow isTvShow={true} key={key} title={item.title} items={item.data} />
+                            ))}
+                        </section> : <></>
+                    }
+
+                    {
+                        series.length ? <section className="lists">
+                            {[series].map((item, key) => (
+                                <MovieRow key={key} isTvShow={true} title={"Series"} items={item} />
+                            ))}
+                        </section> : <></>
+                    } */}
+                    {/* <HomeFooter /> */}
+                </div>
+                // : <Search />
+            }
+        </Layout>
+    );
+}
+export default Dashboad;
+
